66dac87e302d5dc666326da82ab2f5c4516a109f
HuriyehBabak <120142026+HuriyehBabak@users.noreply.github.com>
2024-04-15 22:11:36 +0200

    Update test.py

--- ex1/test.py
+++ ex1/test.py
@@ -1,5 +1,13 @@
 import hashlib
 
-groupid = b"12349876"
+groupid = b"hhhhhh"
 
-print(hashlib.sha224(groupid).hexdigest())
+with open("ergebnispins.txt", "r") as file:
+    pins = file.read().strip()
+
+
+data = groupid + pins.encode('utf-8')
+
+hashed = hashlib.sha224(data).hexdigest()
+
+print(hashed)
