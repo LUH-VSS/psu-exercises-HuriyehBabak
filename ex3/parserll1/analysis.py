655bf9e39605fda0b4dead290bfe33df0aecc410
p0uryRokn <111652867+p0uryRokn@users.noreply.github.com>
2024-04-30 09:16:42 +0200

    Update analysis.py

--- ex3/parserll1/analysis.py
+++ ex3/parserll1/analysis.py
@@ -12,9 +12,24 @@ class LL1Analysis:
         Returns true if all non-terminals within word can result
         in the empty word via their derivations.
         """
-        assert isWord(word), "{} must be a word".format(word)
+        assert isWord(word), "{} is no word".format(word)
 
-        return False  # just placeholder
+        if not visited:
+            visited = set()
+
+        for symbol in word:
+            if isinstance(symbol, Epsilon):
+                continue
+            elif isinstance(symbol, Terminal):
+                return False
+            elif isinstance(symbol, NonTerminal):
+                if symbol in visited:
+                    continue
+                visited.add(symbol)
+                if not any(self.EPS(rule.rhs, visited) for rule in symbol.rules):
+                    return False
+
+        return True
 
     def FIRST(self, word: Word, visited: Optional[Set[NonTerminal]] = None) -> Set[Terminal]:
         """
@@ -23,9 +38,30 @@ class LL1Analysis:
         The FIRST set contains all terminals which occur first at the right side of a derivation
         of a given non-terminal.
         """
-        assert isWord(word), "{} must be a word".format(word)
+        assert isWord(word), "{} is no word".format(word)
 
-        return set()  # just placeholder
+        first = set()
+
+        if not visited:
+            visited = set()
+
+        for symbol in word:
+            if isinstance(symbol, Epsilon):
+                continue
+            elif isinstance(symbol, Terminal):
+                first.add(symbol)
+                break
+            elif isinstance(symbol, NonTerminal):
+                if symbol in visited:
+                    continue
+                visited.add(symbol)
+                for rule in symbol.rules:
+                    for x in self.FIRST(rule.rhs, visited):
+                        first.add(x)
+                if not any(self.EPS(rule.rhs) for rule in symbol.rules):
+                    break
+        
+        return first
 
     def FOLLOW(self, symbol: NonTerminal, visited: Optional[Set[NonTerminal]] = None) -> Set[Terminal]:
         """
@@ -34,9 +70,25 @@ class LL1Analysis:
         The FOLLOW set contains all terminals which occur right after the given non-terminal symbol
         when derivating from the start symbol S.
         """
-        assert isinstance(symbol, NonTerminal), "{} must be a NonTerminal".format(symbol)
+        assert isinstance(symbol, NonTerminal),\
+            "{} is no NonTerminal".format(symbol)
 
-        return set()  # just placeholder
+        follow = set()
+
+        if not visited:
+            visited = set()
+
+        for rule in self.grammar.rules:
+            for index, value in [(i, v) for i, v in enumerate(rule.rhs) if v == symbol]:
+                rest = rule.rhs[(index + 1):]
+                for x in self.FIRST(rest):
+                    follow.add(x)
+                if self.EPS(rest) and rule.lhs not in visited:
+                    visited.add(rule.lhs)
+                    for x in self.FOLLOW(rule.lhs, visited):
+                        follow.add(x)
+
+        return follow
 
     def PREDICT(self, rule: Rule) -> Set[Terminal]:
         """
@@ -47,6 +99,15 @@ class LL1Analysis:
         If a, however, can become the empty word via derivation then
         the PREDICT set also contains the FOLLOW set of the non-terminal A.
         """
-        assert isinstance(rule, Rule), "{} must be a rule".format(rule)
+        assert isinstance(rule, Rule), \
+            "{} is no rule".format(rule)
 
-        return set()  # just placeholder
+        predict = set()
+
+        for x in self.FIRST(rule.rhs):
+            predict.add(x)
+        if self.EPS(rule.rhs):
+            for x in self.FOLLOW(rule.lhs):
+                predict.add(x)
+        
+        return predict
